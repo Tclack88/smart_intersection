60b290bc3dd9a7a737e1ebded2e21c6172f7d3ef
Trevor Clack <tclack@aol.com>
2020-05-31 14:43:57 -0700

    combined simulation initialization and global variables into their own class

--- transport.py
+++ transport.py
@@ -8,16 +8,17 @@ GREEN = (0,255,0)
 BLACK = (0,0,0)
 FPS = 30
 
-class Car(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((40,15))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.center = (0, HEIGHT/2)
-
-    def update(self):
-        self.rect.x += 5
+# Keeping this here if I have to go the "sprite" route
+# class Car(pygame.sprite.Sprite):
+#     def __init__(self):
+#         pygame.sprite.Sprite.__init__(self)
+#         self.image = pygame.Surface((40,15))
+#         self.image.fill(GREEN)
+#         self.rect = self.image.get_rect()
+#         self.rect.center = (0, HEIGHT/2)
+# 
+#     def update(self):
+#         self.rect.x += 5
 
 class Car:
     # direction: right, down for now
@@ -62,45 +63,60 @@ class Road:
         pygame.draw.rect(screen, (100,100,100), (self.x, self.y, self.w, self.h))
 
 
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Smart Intersection Simulation")
-clock = pygame.time.Clock()
+class Simulation:
+    def __init__(self):
+        self.running = True
+        self.size = self.WIDTH, self.HEIGHT = 1000, 1000
+        self.screen = pygame.display.set_mode(self.size)
 
-# Initialize
-car = Car(0, HEIGHT/2)
-c2 = Car(WIDTH/2, 0, 'd')
-rh = Road('h',.5)
-rv = Road('v',.5)
-# Game loop
-running = True
-while running:
-    # keep loop running at the right speed
-    clock.tick(FPS)
-    # Process input (events)
-    for event in pygame.event.get():
-        # check for closing window
-        if event.type == pygame.QUIT:
-            running = False
-
-    #all_sprites = pygame.sprite.Group()
-    #all_sprites.add(car)
-
-    # Update
+    def on_init(self):
+        pygame.init()
+        pygame.mixer.init()
+        pygame.display.set_caption("Smart Intersection Simulation")
+        self.clock = pygame.time.Clock()
+        self.FPS = 30
     
-    #all_sprites.update()
-    car.update()
-    c2.update()
-    pygame.display.update()
-    # Draw / render
-    screen.fill(BLACK)
-    rh.render(screen)
-    rv.render(screen)
-    car.render(screen)
-    c2.render(screen)
-    #all_sprites.draw(screen)
-    # *after* drawing everything, flip the display
-    #pygame.display.flip()
+    def object_init(self):
+        self.cars = [Car(0, HEIGHT/2), Car(WIDTH/2, 0, 'd')]
+        self.roads = [Road('h',.5), Road('v',.5)]
 
-pygame.quit()
+    def on_loop(self):
+        while self.running:
+            # keep loop running at the right speed
+            self.clock.tick(self.FPS)
+            # Process input (events)
+            for event in pygame.event.get():
+                # check for closing window
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            #all_sprites = pygame.sprite.Group()
+            #all_sprites.add(car)
+
+            # Update
+            
+            #all_sprites.update()
+            for car in self.cars:
+                car.update()
+            pygame.display.update()
+            # Draw / render
+            self.screen.fill(BLACK)
+            for road in self.roads:
+                road.render(self.screen)
+            for car in self.cars:
+                car.render(self.screen)
+            #all_sprites.draw(screen)
+            # *after* drawing everything, flip the display
+            #pygame.display.flip()
+
+        pygame.quit()
+
+    def execute(self):
+        self.on_init()
+        self.object_init()
+        self.on_loop()
+
+
+if __name__ == "__main__":
+    simulation = Simulation()
+    simulation.execute()
